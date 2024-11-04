de26a9948ae6042bc18a03f55c164fe0cbe53784
Manuela Estrada <manuesmanager@gmail.com>
2024-11-04 11:29:47 -0500

    Agregar servicios y lib aplicaciones e utilidades

---
+++ Catalogo/lib_aplicaciones/Interfaces/ICategoriasAplicacion.cs
@@ -0,0 +1,14 @@
+﻿using lib_entidades.Modelos;
+
+namespace lib_aplicaciones.Interfaces
+{
+    public interface ICategoriasAplicacion
+    {
+        void Configurar(string string_conexion);
+        List<Categorias> Buscar(Categorias entidad, string tipo);
+        List<Categorias> Listar();
+        Categorias Guardar(Categorias entidad);
+        Categorias Modificar(Categorias entidad);
+        Categorias Borrar(Categorias entidad);
+    }
+}
---
+++ Catalogo/lib_aplicaciones/Interfaces/IPublicacionesAplicacion.cs
@@ -0,0 +1,14 @@
+﻿using lib_entidades.Modelos;
+
+namespace lib_aplicaciones.Interfaces
+{
+    public interface IPublicacionesAplicacion
+    {
+        void Configurar(string string_conexion);
+        List<Publicaciones> Buscar(Publicaciones entidad, string tipo);
+        List<Publicaciones> Listar();
+        Publicaciones Guardar(Publicaciones entidad);
+        Publicaciones Modificar(Publicaciones entidad);
+        Publicaciones Borrar(Publicaciones entidad);
+    }
+}
