97ac4a58e74d74d65aff2867a786a8be640c88d8
Emanuel <ema795ps4@gmail.com>
2024-11-10 23:05:03 -0500

    Presentaciones

---
+++ Catalogo/asp_presentacion/Startup.cs
@@ -0,0 +1,57 @@
+﻿using lib_comunicaciones.Implementaciones;
+using lib_comunicaciones.Interfaces;
+using lib_presentaciones.Implementaciones;
+using lib_presentaciones.Interfaces;
+
+namespace asp_presentacion
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration? Configuration { set; get; }
+
+        public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
+        {
+            // Comunicaciones
+            services.AddScoped<IEstadosComunicacion, EstadosComunicacion>();
+            services.AddScoped<IFabricantesComunicacion, FabricantesComunicacion>();
+            services.AddScoped<IImagenesComunicacion, ImagenesComunicacion>();
+            services.AddScoped<IProductosComunicacion, ProductosComunicacion>();
+            services.AddScoped<IPublicacionesComunicacion, PublicacionesComunicacion>();
+            services.AddScoped<ICategoriasComunicacion, CategoriasComunicacion>();
+            // Presentaciones
+            services.AddScoped<IEstadosPresentacion, EstadosPresentacion>();
+            services.AddScoped<IFabricantesPresentacion, FabricantesPresentacion>();
+            services.AddScoped<IImagenesPresentacion, ImagenesPresentacion>();
+            services.AddScoped<IProductosPresentacion, ProductosPresentacion>();
+            services.AddScoped<IPublicacionesPresentacion, PublicacionesPresentacion>();
+            services.AddScoped<ICategoriasPresentacion, CategoriasPresentacion>();
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddRazorPages();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+            });
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.UseSession();
+            app.Run();
+        }
+    }
+}
