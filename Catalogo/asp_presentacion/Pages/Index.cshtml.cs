38c84386cc0cb0d260009446905a17c2bf757f18
Emanuel <ema795ps4@gmail.com>
2024-11-25 03:28:15 -0500

    Se agregan entidades para Roles y Usuarios

--- Catalogo/asp_presentacion/Pages/Index.cshtml.cs
+++ Catalogo/asp_presentacion/Pages/Index.cshtml.cs
@@ -1,29 +1,80 @@
+using lib_utilidades;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace asp_presentacion.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        public bool EstaLogueado = false;
+        [BindProperty] public string? Email { get; set; }
+        [BindProperty] public string? Contraseña { get; set; }
+        [BindProperty] public string? Nombre { get; set; }
 
         public void OnGet()
         {
+            var variable_session = HttpContext.Session.GetString("Usuario");
+            if (!String.IsNullOrEmpty(variable_session))
+            {
+                EstaLogueado = true;
+                return;
+            }
+        }
+
+        public void OnPostBtClean()
+        {
             try
             {
-                //var variable_session = HttpContext.Session.GetString("usuario");
-                //if (String.IsNullOrEmpty(variable_session))
-                //    HttpContext.Session.SetString("usuario", "Pruebas");
-                ViewData["Logeado"] = true;
+                Email = string.Empty;
+                Contraseña = string.Empty;
+                Nombre = string.Empty;
             }
             catch (Exception ex)
             {
-                ViewData["Logeado"] = false;
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtEnter()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Email) &&
+                    string.IsNullOrEmpty(Contraseña) && 
+                    string.IsNullOrEmpty(Nombre))
+                {
+                    OnPostBtClean();
+                    return;
+                }
+
+                if ("Usuario.123" != Email + "." + Contraseña)
+                {
+                    OnPostBtClean();
+                    return;
+                }
+                ViewData["Logged"] = true;
+                HttpContext.Session.SetString("Usuario", Email!);
+                EstaLogueado = true;
+                OnPostBtClean();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtClose()
+        {
+            try
+            {
+                HttpContext.Session.Clear();
+                HttpContext.Response.Redirect("/");
+                EstaLogueado = false;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
             }
         }
     }
